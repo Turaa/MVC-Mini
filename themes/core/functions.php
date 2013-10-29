d1b183f56e473f3411753c82c9e1f274b5ba0f98
Tura Turesson <tura.in.wonderland@hotmail.com>
2013-10-29 14:19:14 +0100

    Added the Mini-Framework

---
+++ themes/core/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Helpers for the template file.
+*/
+
+/**
+* Add static entries for use in the template file.
+*/
+$mi->data['header'] = 'Mini';
+$mi->data['slogan'] = 'A PHP-based MVC-inspired CMF';
+$mi->data['favicon'] = theme_url('logo_80x80.png');
+$mi->data['logo'] = theme_url('logo_80x80.png');
+$mi->data['logo_width'] = 80;
+$mi->data['logo_height'] = 80;
+$mi->data['footer'] = <<<EOD
+<p>Footer: &copy; Mini by Tura, 2013</p>
+EOD;
