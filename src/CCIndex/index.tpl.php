f8381e048edda22102d80def80b1cdd4a99f1ff0
Turaa <tura.in.wonderland@hotmail.com>
2013-11-09 10:41:20 +0100

    Updated CCIndex

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -1,8 +1,8 @@
 <h1>Index Controller</h1>
-<p>Welcome to Mini index controller.</p>
+<p>Welcome to MVC-Mini index controller.</p>
 
 <h2>Download</h2>
-<p>You can download Mini from github.</p>
+<p>You can download MVC-Mini from github.</p>
 <blockquote>
 <code>git clone git://github.com/Turaa/MVC-Mini.git</code>
 </blockquote>
@@ -10,12 +10,10 @@
 
 <h2>Installation</h2>
 <p>First you have to make the data-directory writable. This is the place where Mini needs
-to be able to write and create files.</p>
-<blockquote>
-<code>cd MVC-Mini; chmod 777 site/data</code>
-</blockquote>
+to be able to write and create files. Easiest way to do this is to do it in your ftp-program. (Example FileZilla). Right-click on the folder "data" in site/data
+and change file permissions to 777. Make sure to change the .sqlite file in "data" to 777 too.</p>
 
-<p>Second, Mini has some modules that need to be initialised. You can do this through a
+<p>Second, MVC-Mini has some modules that need to be initialised. You can do this through a
 controller. Point your browser to the following link.</p>
 <blockquote>
 <a href='<?=create_url('module/install')?>'>module/install</a>
